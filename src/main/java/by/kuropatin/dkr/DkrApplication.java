d8868df9d3169e841b555d1b769f636fb9454500
Pavel Puropatin <kuropatin.pv@gmail.com>
2022-06-04 18:49:06 +0300

    Initial commit

---
+++ src/main/java/by/kuropatin/dkr/DkrApplication.java
@@ -0,0 +1,12 @@
+package by.kuropatin.dkr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DkrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DkrApplication.class, args);
+    }
+}
---
+++ src/test/java/by/kuropatin/dkr/DkrApplicationTests.java
@@ -0,0 +1,15 @@
+package by.kuropatin.dkr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.persistence.EntityManager;
+
+@SpringBootTest
+class DkrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
